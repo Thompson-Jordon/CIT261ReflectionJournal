9092f9c9bea7fe39fb1752cb4b6d47694b62ee8d
Jordon Thompson <tho17088@byui.edu>
2019-10-07 21:30:20 -0600

    fix mistake

--- scripts/json.js
+++ scripts/json.js
@@ -5,7 +5,7 @@ function parseJSON() {
   var text =
     '{"ball": {"color": "green", "size": "small", "typeOf": "soccer"}}';
   obj = JSON.parse(text);
-  var story = `Jimmy loves to play ${obj.ball.typeOf} with his ${obj.ball.size} ${obj.ball.color} ${obj.ball}.`;
+  var story = `Jimmy loves to play ${obj.ball.typeOf} with his ${obj.ball.size} ${obj.ball.color} ball.`;
 
   //place story in html
   document.getElementById("story").innerHTML = story;
