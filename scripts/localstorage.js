31d608692b4e4a0001e8ea35206f6ee6266c90a7
Jordon Thompson <jothompson@crescentpointenergy.com>
2019-10-15 13:46:09 -0600

    work on local storage

---
+++ scripts/localstorage.js
@@ -0,0 +1,23 @@
+//You can set a variable to your local storage
+let myStorage = window.localStorage;
+let tempStorage = window.sessionStorage;
+
+document.getElementById("store").value = myStorage.username;
+
+function storeUsername() {
+  let username = document.getElementById("store").value;
+  let key = document.getElementById("key").value;
+  let val = document.getElementById("value").value;
+  //store username is localStorage
+  myStorage.username = username;
+  myStorage.setItem(key, val);
+}
+
+function removeUsername() {
+  //Remove username from long term history
+  myStorage.removeItem.username;
+}
+
+function clearStorage() {
+  myStorage.clear();
+}
