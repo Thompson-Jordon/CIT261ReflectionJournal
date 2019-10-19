ed79553828015ed35422d5a98cf6bb7b9084cfff
Jordon Thompson <tho17088@byui.edu>
2019-10-19 10:10:30 -0600

    work on ajax

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -2,14 +2,16 @@ const key = "566212c3f3fa2dd43a6047055529f569";
 
 function getWeather() {
   let city = document.getElementById("city").value;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=${key}`
-  
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&appid=${key}`;
+
   let xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if(this.readyState = 4 && this.status == 200) {
+    if ((this.readyState = 4 && this.status == 200)) {
       const obj = JSON.parse(this.responseText);
       document.getElementById("location").innerHTML = city;
-      document.getElementById("temp").innerHTML = `Current Temperature: ${obj.main.temp} °F`;
+      document.getElementById(
+        "temp"
+      ).innerHTML = `Current Temperature: ${obj.main.temp} °F`;
     }
   };
   xhttp.open("POST", url, true);
