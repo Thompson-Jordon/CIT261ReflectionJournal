705d496427c7d01fa7c6651b4879d399f9681085
Jordon Thompson <tho17088@byui.edu>
2019-11-04 21:28:32 -0700

    finish html5 tags

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -1,43 +1,31 @@
-let canvas = /** @type {HTMLCanbasElement} */ (document.getElementById('canvas'));
-//context = document.getElementById('canvas').getContext("2d");
-let clickX = new Array();
-let clickY = new Array();
-let clickDrag = new Array();
-let paint;
+let canvas = document.getElementById("myCanvas");
+let context = /** @type {CanvasRenderingContext2D} */ (canvas.getContext("2d"));
+let blockW = 30;
+let boardW = 240;
 
-function addClick(x, y, dragging) {
-   clickX.push(x);
-   clickY.push(y);
-   clickDrag.push(dragging);
+for (i = 0; i < 8; i++) {
+  for (j = 0; j < 8; j++) {
+    context.fillStyle = "red";
+    context.moveTo(0, blockW * j);
+    context.lineTo(boardW, blockW * j);
+    context.stroke();
+
+    context.moveTo(blockW * i, 0);
+    context.lineTo(blockW * i, boardW);
+    context.stroke();
+    let left = 0;
+
+    // fill in every other square
+    for (let a = 0; a < 8; a++) {
+      for (let b = 0; b < 8; b += 2) {
+        startX = b * blockW;
+        if (a % 2 == 0) {
+          startX = (b + 1) * blockW;
+          context.fillRect(startX + left, (a * blockW), blockW, blockW);
+        } else {
+          context.fillRect(startX + left, (a * blockW), blockW, blockW);
+        }
+      }
+    }
+  }
 }
-
-function redraw() {
-   // clears the canvas
-   canvas.clearRect(0,0,canvas.canvas.width, canvas.canvas.height);
-
-   canvas.str
-}
-
-canvas.mousedown(function(e) {
-   let mouseX = e.pageX - this.offsetLeft;
-   let mouseY = e.pageY - this.offsetTop;
-
-   paint = true;
-   addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-   redraw();
-});
-
-canvas.mousemove(function(e) {
-   if(paint) {
-      addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-      redraw();
-   }
-});
-
-canvas.mouseup(function(e) {
-   paint = false;
-});
-
-canvas.mouseleave(function(e){
-   paint = false;
-});
